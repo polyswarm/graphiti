1231e113b925097fff2910700bfd0ec1158ac0e7
Michael Bradford <michael.bradford@polyswarm.io>
2025-09-17 15:33:05 -0700

    read aws env

--- graphiti_core/driver/neptune_driver.py
+++ graphiti_core/driver/neptune_driver.py
@@ -17,6 +17,7 @@ limitations under the License.
 import asyncio
 import datetime
 import logging
+import os
 from collections.abc import Coroutine
 from typing import Any
 
@@ -111,8 +112,7 @@ aoss_indices = [
 class NeptuneDriver(GraphDriver):
     provider: GraphProvider = GraphProvider.NEPTUNE
 
-    def __init__(self, host: str, aoss_host: str, port: int = 8182, aoss_port: int = 443, aws_access_key_id=None,
-                 aws_secret_access_key=None, aws_region_name=None, use_https=True):
+    def __init__(self, host: str, aoss_host: str, port: int = 8182, aoss_port: int = 443, use_https=True):
         """This initializes a NeptuneDriver for use with Neptune as a backend
 
         Args:
@@ -127,9 +127,15 @@ class NeptuneDriver(GraphDriver):
         if host.startswith('neptune-db://'):
             # This is a Neptune Database Cluster
             endpoint = host.replace('neptune-db://', '')
-            self.client = NeptuneGraph(endpoint, port, aws_access_key_id=SecretStr(aws_access_key_id),
-                                       aws_secret_access_key=SecretStr(aws_secret_access_key),
-                                       region_name=aws_region_name, use_https=use_https)
+            aws_access_key_id = os.getenv('NEPTUNE_AWS_ACCESS_KEY_ID')
+            aws_secret_access_key = os.getenv('NEPTUNE_AWS_SECRET_ACCESS_KEY')
+            aws_region_name = os.getenv('NEPTUNE_AWS_REGION')
+            if aws_access_key_id and aws_secret_access_key and aws_region_name:
+                self.client = NeptuneGraph(endpoint, port, aws_access_key_id=SecretStr(aws_access_key_id),
+                                           aws_secret_access_key=SecretStr(aws_secret_access_key),
+                                           region_name=aws_region_name, use_https=use_https)
+            else:
+                self.client = NeptuneGraph(endpoint, port, use_https=use_https)
             logger.debug('Creating Neptune Database session for %s', host)
         elif host.startswith('neptune-graph://'):
             # This is a Neptune Analytics Graph
@@ -148,9 +154,9 @@ class NeptuneDriver(GraphDriver):
         self.aoss_client = OpenSearch(
             hosts=[{'host': aoss_host, 'port': aoss_port}],
             http_auth=Urllib3AWSV4SignerAuth(
-                session.get_credentials(), aws_region_name, 'es'
+                session.get_credentials(), aws_region_name or 'us-east-2', 'es'
             ),
-            use_ssl=True,
+            use_ssl=use_https,
             verify_certs=True,
             connection_class=Urllib3HttpConnection,
             pool_maxsize=20,
